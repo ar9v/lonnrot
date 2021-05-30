2ace647700bb23e4366a40a6eb6f9e0d18bd497a
Ricardo Vela <aricav96@gmail.com>
2021-05-30 17:57:53 -0500

    Load instructions. Proper vm struct

--- src/vm/compile-file-image.rkt
+++ src/vm/compile-file-image.rkt
@@ -1,37 +1,18 @@
 #lang racket
 (provide main)
-(require "../parse.rkt" "compile-vm.rkt" "../mparse.rkt" a86)
+(require "../parse.rkt" "compile-vm.rkt" "../mparse.rkt" "ast.rkt" a86)
 
 ;; main: String -> Void
 ;; So, in summary:
 ;; 1. String -> S-exps (Reader)
 ;; 2. S-exps -> AST (parse)
-;; 3. AST -> x86 AST (compile)
-;; 4. x86 AST -> NASM (asm-string)
+;; 3. AST -> a86 AST (compile)
+;; Result: a list of vectors
 (define (main filename)
   (let ([filename-port (open-input-file filename)])
     (begin
-      ;; (read-line filename-port) NOTE: uncomment if using the Racket reader
       (displayln
-       (map struct->list
         (compile
          (parse
-          ;; (read filename-port))))) NOTE: uncomment if using the Racket reader
-          (mread (port->string filename-port) filename)))))
+          (mread (port->string filename-port) filename))))
       (close-input-port filename-port))))
-
-(define (struct->list quad)
-  (match quad
-    [(Extern s) `(Extern ,s)]
-    [(Label s)  `(Label ,s)]
-    [(Mov r1 r2) `(Mov ,r1 ,r2)]
-    [(Add r1 r2) `(Add ,r1 ,r2)]
-    [(Push r)    `(Push ,r)]
-    [(Pop r)     `(Pop ,r)]
-    [(And r e)     `(And ,r ,e)]
-    [(Cmp r e)   `(Cmp ,r ,e)]
-    [(Jmp l)     `(Jmp ,l)]
-    [(Je l)      `(Je ,l)]
-    [(Jne l)     `(Jne ,l)]
-    [(Ret)       `(Ret)]
-    [_           '(MISSING OP)]))
--- src/vm/vm.rkt
+++ src/vm/vm.rkt
@@ -13,30 +13,51 @@
 ;; Flags:
 ;; For now we only need the EQ flag
 
-(struct reg (val))
-(struct vm (ip sp regs))
-
+(struct vm
+  (ip sp regs code s h)
+  #:mutable)
 
 ;; main: String -> Answer
 ;; Main reads an image file, which is a list of a86 ops
 ;; and calls the VM's run function
 (define (main image-file)
   (let ([filename-port (open-input-file image-file)])
-    (run (read filename-port)
-         1)))
+    (run
+     (vm
+      0
+      1
+      (make-hash '((rax . 0) (rbx . 0) (rcx . 0) (rdx . 0) (r8 . 0) (r9 . 0) (rdi . 0)))
+      (read filename-port)
+      '()
+      '()))))
 
-
-;; run: [ASM Ops] -> Answer
-(define (run code pc)
-  (cond [(empty? code) "Program finished successfully"]
+;; run: VM -> Answer
+;; run takes a vm struct, which represents a state at a given time
+;; and executes the current code until the final Ret instruction has
+;; been found
+(define (run vm)
+  (cond [(empty? (vm-code vm)) "finished"]
         [else
-         (op-dispatch (car code))
-         (run (cdr code) pc)]))
+         (displayln (load-op (car (vm-code vm))))
+         (set-vm-code! vm (cdr (vm-code vm)))
+         (run vm)]))
+
+(define (load-op ins)
+  (vector-ref ins 0))
 
 
-(define (op-dispatch op)
-  (match op
-    [(list 'Extern s) (displayln "Found extern")]
-    [(list 'Label l) (displayln "Found label")]
-    [(list 'Mov r1 r2) (displayln "Found mov")]
-    [_ (displayln "op does not exist")]))
+;; (Push x)
+;; -> Decrements rsp (Sub rsp 8)
+;; -> Moves `x` to the top frame (Mov (Offset rsp 0) x)
+;;
+;; (Pop x)
+;; -> Moves what is pointed to by rsp onto x (Mov x (Offset rsp 0))
+;; -> Increments rsp (Add rsp 8)
+;;
+;; (Call f)
+;; -> push the return address (i.e. where we left off)
+;; -> jumps to label pointed to by f
+;;
+;; (Ret)
+;; -> pops the return address from the stack
+;; -> jumps to this address
