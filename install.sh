c072b5fb59446daebd8cb81fbba834ec0d863de9
Ricardo Vela <aricav96@gmail.com>
2021-06-02 23:25:51 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 if [ -d ~/bin ]
 then
     echo Home bin dir exists... creating symlink...
-    ln -s src/cli/compass ~/bin/compass
+    ln -s $PWD/src/cli/compass ~/bin/compass
 else
     echo Home bin dir does not exist, will try to create symlink on /usr/bin/
-    sudo ln -s src/cli/compas /usr/bin/compass
+    sudo ln -s $PWD/src/cli/compas /usr/bin/compass
 fi
 
 echo Done
